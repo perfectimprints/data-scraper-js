c3772a7348253c8cc5289096ed495acad5cac8f2
Patrick Black Jr <info@patrickblackjr.com>
2018-11-02 15:35:50 -0500

    Add name, sku, title, shortDesc. Broken: pricing.

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const request = require('request');
+const cheerio = require('cheerio');
 
-let url =
-  "https://www.perfectimprints.com/promos/PI-AVL-AD-WO/Custom-Laser-Cut-Wood-Ornaments.html?trackcode=googleBase&keyword_session_id=vt~adwords%7ckt~%7cmt~%7cta~284645420109&_vsrefdom=wordstream";
+let url = 'https://www.perfectimprints.com/promos/PI-AVL-AD-WO/Custom-Laser-Cut-Wood-Ornaments.html';
 
-request(url, { json: false }, (err, res, body) => {
+request(url, (err, res, body) => {
+
   if (err) return console.error(err);
 
-  
+  const $ = cheerio.load(body);
 
+  let name = $('.details_product_name>h1').text();
+  let sku = $('.sku_value').text();
+  let image = $('meta[property="og:image"]').attr('content');
+  let title = $('meta[property="og:title"]').attr('content');
+  let shortDesc = $('meta[name=description]').attr('content');
+  let pricing = $('th').text().replace(/\t/g, '').replace(/\n/g, '').split(' ').filter(function (el) {
+    return el != undefined;
+  })
+
+  if (!shortDesc) {
+    shortDesc = 'Get custom {{ name }} from PerfectImprints.com.'
+  }
+
+  console.log(Array.from(pricing))
 });
