0f4de7da50d923079f2cb53ad1fe8ddb615521e5
Patrick Black Jr <info@patrickblackjr.com>
2018-11-02 16:00:34 -0500

    Pricing scraping updates (fixes #1)

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
-let url = 'https://www.perfectimprints.com/promos/PI-AVL-AD-WO/Custom-Laser-Cut-Wood-Ornaments.html';
+// let url = 'https://www.perfectimprints.com/promos/PI-AVL-AD-WO/Custom-Laser-Cut-Wood-Ornaments.html';
+let url = 'https://www.perfectimprints.com/promos/PI-159-G465-58706481/ADULT-Performance-Adult-HoodedT-Shirt.html'
 
 request(url, (err, res, body) => {
 
@@ -14,13 +15,11 @@ request(url, (err, res, body) => {
   let image = $('meta[property="og:image"]').attr('content');
   let title = $('meta[property="og:title"]').attr('content');
   let shortDesc = $('meta[name=description]').attr('content');
-  let pricing = $('th').text().replace(/\t/g, '').replace(/\n/g, '').split(' ').filter(function (el) {
-    return el != undefined;
-  })
+  let longDesc = $('.details_long_desc').html();
+  let price = $('meta[property="product:price:amount"]').attr('content');
 
   if (!shortDesc) {
     shortDesc = 'Get custom {{ name }} from PerfectImprints.com.'
   }
 
-  console.log(Array.from(pricing))
 });
